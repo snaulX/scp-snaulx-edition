733670b704148854d5d9039df14c86fa63a2b977
snaulX <alexandergunger666@gmail.com>
2020-08-10 20:50:37 +0300

    Add simple SCPDC scripts

---
+++ SCP snaulX Edition/Assets/SCPDC/SimpleClient.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Threading;
+using UnityEngine;
+
+public class SimpleClient : MonoBehaviour {
+    public string hostname = "localhost";
+    public int port = 1215;
+    public SimpleParser parser;
+    public TcpClient client;
+    public Thread readThread;
+    public Thread writeThread;
+    public bool connected = false;
+    public bool error = false;
+    public List<byte[]> clientQueue;
+    public List<byte[]> sendQueue;
+
+    public bool connect = false;
+    public bool disconnect = false;
+
+    private void Start() {
+        clientQueue = new List<byte[]>();
+    }
+
+    private void Update() {
+        if ( connect ) { connect = false; Connect( hostname, port ); }
+        if ( disconnect ) { disconnect = false; Disconnect(); }
+
+        if ( clientQueue.Count == 0 ) return;
+        
+        while ( clientQueue.Count > 0 ) {
+            try {
+                if ( parser != null ) {
+                    parser.Parse( clientQueue[ 0 ] );
+                }
+            } catch ( Exception e ) {
+                Debug.LogError( $"QueueException:\n{e.Message}" );
+            } finally {
+                clientQueue.RemoveAt( 0 );
+            }
+        }
+    }
+
+    public void Connect( string hostname, int port ) {
+        try {
+            client = new TcpClient( hostname, port );
+            readThread = new Thread( ClientThread );
+            readThread.Start();
+            writeThread = new Thread( ClientSend );
+            writeThread.Start();
+            connected = true;
+        } catch ( Exception e ) {
+            Debug.LogError( e.Message );
+        }
+    }
+
+    public void Disconnect() {
+        try {
+            client.Client.Shutdown( SocketShutdown.Both );
+        } catch ( Exception e ) {
+            Debug.LogError( e.Message );
+        } finally {
+            try { readThread.Abort(); } finally { }
+            try { writeThread.Abort(); } finally { }
+            connected = false;
+            error = false;
+            client.Client.Close();
+        }
+    }
+
+    public void ClientThread() {
+        while( connected ) {
+            try {
+                if ( client.Available > 0 && !error ) {
+                    byte[] buffer = new byte[ 65535 ];
+                    int byteCount = client.Client.Receive( buffer );
+                    if ( byteCount != 0 && byteCount != -1 ) {
+                        byte[] data = new byte[ byteCount ];
+                        Buffer.BlockCopy( buffer, 0, data, 0, byteCount );
+                        if ( data[ 0 ] == 0xFF && data[ 1 ] == 0xFF && data[ 2 ] == 0xFF && data[ 3 ] == 0xFF ) {
+                            continue;
+                        } else {
+                            clientQueue.Add( data );
+                        }
+                    }
+                } else {
+                    Thread.Sleep( 16 );
+                }
+            } catch( SocketException se ) {
+                Debug.LogError( $"SocketException:\n{se.Message}" );
+                byte retryCount = 0;
+                // Try to connect again
+                while ( retryCount < 4 ) {
+                    try {
+                        client.Client.Send( BitConverter.GetBytes( 0xFFFFFFFF ) );
+                        retryCount = 0;
+                        error = false;
+                        break;
+                    } catch ( SocketException ) {
+                        Debug.Log( $"Try {retryCount}..." );
+                    } catch( Exception ee ) {
+                        Debug.Log( $"Internal exception on try:\n{ee.Message}" );
+                    }
+                    retryCount++;
+                    Thread.Sleep( 1000 );
+                }
+                if ( retryCount == 4 ) {
+                    Debug.LogError( "Failed to connect after 3 retries" );
+                    connected = false;
+                    return;
+                }
+            } catch ( Exception e ) {
+                Debug.LogError( $"InternalException\n{e.Message}" );
+            }
+        }
+    }
+
+    public void ClientSend() {
+        while( connected ) {
+            try {
+                if ( sendQueue.Count > 0 && !error ) {
+                    client.Client.Send( sendQueue[ 0 ] );
+                    sendQueue.RemoveAt( 0 );
+                } else {
+                    Thread.Sleep( 16 );
+                }
+            } catch ( SocketException se ) {
+                error = true;
+                Debug.LogError( $"SocketException:\n{se.Message}" );
+                byte retryCount = 0;
+                // Try to connect again
+                while ( retryCount < 4 ) {
+                    try {
+                        client.Client.Send( BitConverter.GetBytes( 0xFFFFFFFF ) );
+                        retryCount = 0;
+                        error = false;
+                        break;
+                    } catch ( SocketException ) {
+                        retryCount++;
+                        Debug.Log( $"Try {retryCount}..." );
+                        Thread.Sleep( 1000 );
+                    } catch ( Exception ee ) {
+                        Debug.Log( $"Internal exception on try:\n{ee.Message}" );
+                        Thread.Sleep( 1000 );
+                    }
+                }
+                if ( retryCount == 4 ) {
+                    Debug.LogError( "Failed to connect after 3 retries" );
+                    connected = false;
+                    return;
+                }
+            } catch ( Exception e ) {
+                Debug.LogError( $"InternalException\n{e.Message}" );
+            }
+        }
+    }
+}
---
+++ SCP snaulX Edition/Assets/SCPDC/SimpleParser.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleParser : MonoBehaviour {
+    [Serializable]
+    public class TargetID {
+        public int id;
+        public SimplePlayerReceiver receiver;
+        public TargetID( int id, SimplePlayerReceiver receiver ) {
+            this.id = id;
+            this.receiver = receiver;
+        }
+    }
+    internal class ParseCell {
+        public int id = -1;
+        public int command;
+        public byte[] data;
+
+        public ParseCell( int id, int command, byte[] data ) {
+            this.id = id;
+            this.command = command;
+            this.data = data;
+        }
+    }
+    public List<TargetID> targets;
+    public void Parse( byte[] data ) {
+        byte q = ( byte )';';
+        byte[] cell;
+        for( int i = 0, j = 0; i < data.Length; i++ ) {
+            if ( data[ i ] == q ) {
+                cell = new byte[ j + 1 ];
+                int destinationID = BitConverter.ToInt32( data, i - j  );
+                int commandIndex = BitConverter.ToInt32( data, i - j + 4 );
+                Buffer.BlockCopy( data, i - j + 8, cell, 0, cell.Length - 9 );
+                ParseCell parseCell = new ParseCell( destinationID, commandIndex, cell );
+                ProceedParseCell( parseCell );
+                j = 0;
+            } else {
+                j++;
+            }
+        }
+    }
+
+    private void ProceedParseCell( ParseCell parseCell ) {
+        // Do something with commands
+        int targetIDIndex = targets.FindIndex( ( cell ) => cell.id == parseCell.id );
+
+        if ( targetIDIndex != -1 ) {
+            SimplePlayerReceiver receiver = targets[ targetIDIndex ].receiver;
+            switch ( parseCell.command ) {
+                case 0:
+                    receiver.AppendPosition( parseCell.data );
+                    break;
+                case 1:
+                    receiver.AppendRotation( parseCell.data );
+                    break;
+                case 2:
+                    receiver.AppendLocalRotation( parseCell.data );
+                    break;
+                case 3:
+                    receiver.AppendASCII( parseCell.data );
+                    break;
+                case 4:
+                    receiver.AppendUTF8( parseCell.data );
+                    break;
+                case 5:
+                    receiver.AppendAnimationIndex( parseCell.data );
+                    break;
+            }
+            
+        }
+    }
+}
